1bed23602f260a9381a95515456262155efdf6d3
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2023-12-05 14:59:01 -0300

    aprendendo 'for'

---
+++ dia 6/dia6-2.js
@@ -0,0 +1,5 @@
+// ++ -- += -= <= 
+
+for (let contagem = 0; contagem <= 50; contagem+=5) {
+   console.log(contagem)
+}
---
+++ dia 6/dia6-3.js
@@ -0,0 +1,4 @@
+for (let contagem = 50; contagem >= 0; contagem-=5) {
+    console.log(contagem)
+ }
+ 
